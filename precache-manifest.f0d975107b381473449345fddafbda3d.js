8ceeb23a093c40ea2f77d01ae9b9f4dfbcd50447
yaohuangguan <yaob@miamioh.edu>
2019-10-14 21:32:12 +0800

    Updates

---
+++ precache-manifest.f0d975107b381473449345fddafbda3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d321fcc020205da99ae551732109f06",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1a90400e1fd45485a71f",
+    "url": "/monster-rolodex/static/css/main.2d156199.chunk.css"
+  },
+  {
+    "revision": "f2e41403afe2c5f88551",
+    "url": "/monster-rolodex/static/js/2.7e373ac2.chunk.js"
+  },
+  {
+    "revision": "1a90400e1fd45485a71f",
+    "url": "/monster-rolodex/static/js/main.2b196c1f.chunk.js"
+  },
+  {
+    "revision": "cff5d57da39b964a16ea",
+    "url": "/monster-rolodex/static/js/runtime-main.c48da696.js"
+  }
+]);
